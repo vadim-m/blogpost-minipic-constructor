2b5c451da386a93dac69f6fa5eec7b5cc9709658
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2021-02-01 17:11:49 +0300

    Подправил файл скрипта. Сделал правильную кнопку сброса.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,11 @@
+// Элементы со страницы
+/* --------------*/
 const scheme = document.getElementById("scheme");
 const saveBtn = document.getElementById("save-btn");
+
+
+/* СЕКЦИЯ LAYOUT */
+/* --------------*/
 
 // псевдомассив NodeList со списком всех кнопок-стрелок секции
 let openButtons = document.querySelectorAll('.section__open-btn');
@@ -17,53 +23,44 @@ openButtons.forEach((btn) => {
 // функция по переключению класса, чтобы изменить layout картинки
 const changeLayout = () => {
   scheme.classList.toggle('column');
-}
+};
 
 
+/* РАБОТА ГЛАВНОГО СКРИПТА html2canvas */
+/* ------------------------------------ */
 
+// При нажатии на reset-btn - сброс схемы
+const cleanScheme = () => {
+  scheme.innerHTML = `<div class="scheme__title scheme__item">
+                      Title</div>
+                      <div class="scheme__info scheme__item">
+                        <div class="scheme__info-wrap">
+                          <div class="scheme__desc">thesis1</div>
+                          <div class="scheme__desc">thesis2</div>
+                          <div class="scheme__desc">thesis3</div>
+                        </div>
+                      </div>`;
+  resetSaveBtn();
+};
 
-
-
-
-
-
-
-
-
-// reset btn
-const deleteSchemes = () => {
-  scheme.innerHTML = "";
+const resetSaveBtn = () => {
   saveBtn.disabled = true;
   saveBtn.innerHTML = "";
   saveBtn.innerText = "Save";
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // html2canvas => any div as image file
 function doCapture() {
   html2canvas(document.getElementById("scheme")).then(function (canvas) {
-    document.body.style.overflowY = "hidden";
     canvasdata = canvas.toDataURL("image/webp", 1);
+
+    // создаем кнопку, которую потом подставим в
+    // изначально неактивную кнопку saveBtn
     let a = document.createElement("a");
     a.textContent = "Save";
     a.setAttribute("id", "download__link");
     a.download = "схема_уутэ_" + Date.now() + ".webp";
     a.href = canvasdata;
-    console.log(a);
 
     // Делаем кнопку сохранить активной и добавляем в нее ссылку
     saveBtn.disabled = false;
@@ -73,58 +70,58 @@ function doCapture() {
 
     // Добавляем слушатель на ссылку, чтобы при клике очистить схему и выполнить deleteSchemes()
     downloadBtn.addEventListener("click", (e) => {
-      deleteSchemes();
+      cleanScheme();
     });
   });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-// buttons onclick function
-const add = (sym) => {
-  switch (sym) {
-    case "ot":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ot.svg">';
-      break;
-    case "ot-tupik":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ot-tupik.svg">';
-      break;
-    case "gvs":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/gvs.svg">';
-      break;
-    case "gvs-tupik":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/gvs-tupik.svg">';
-      break;
-    case "ts":
-      scheme.innerHTML =
-        scheme.innerHTML +
-        '<img style="width:394px; height:264px" src="./pics/ts.svg">';
-      break;
-    case "ts-pod":
-      scheme.innerHTML =
-        scheme.innerHTML +
-        '<img style="width:394px; height:264px" src="./pics/ts-pod.svg">';
-      break;
-    case "ov":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ov.svg">';
-      break;
-    case "ov-tupik":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ov-tupik.svg">';
-      break;
-    case "up":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/up.svg">';
-      break;
-    case "down":
-      scheme.innerHTML = scheme.innerHTML + '<img src="./pics/down.svg">';
-      break;
-  }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// // buttons onclick function
+// const add = (sym) => {
+//   switch (sym) {
+//     case "ot":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ot.svg">';
+//       break;
+//     case "ot-tupik":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ot-tupik.svg">';
+//       break;
+//     case "gvs":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/gvs.svg">';
+//       break;
+//     case "gvs-tupik":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/gvs-tupik.svg">';
+//       break;
+//     case "ts":
+//       scheme.innerHTML =
+//         scheme.innerHTML +
+//         '<img style="width:394px; height:264px" src="./pics/ts.svg">';
+//       break;
+//     case "ts-pod":
+//       scheme.innerHTML =
+//         scheme.innerHTML +
+//         '<img style="width:394px; height:264px" src="./pics/ts-pod.svg">';
+//       break;
+//     case "ov":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ov.svg">';
+//       break;
+//     case "ov-tupik":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/ov-tupik.svg">';
+//       break;
+//     case "up":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/up.svg">';
+//       break;
+//     case "down":
+//       scheme.innerHTML = scheme.innerHTML + '<img src="./pics/down.svg">';
+//       break;
+//   }
+// };
